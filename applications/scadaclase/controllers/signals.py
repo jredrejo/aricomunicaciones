5955b24c6fe730ead097d6100eae466528ba1eba
José L. Redrejo <jredrejo@gmail.com>
2020-11-06 13:51:19 +0100

    Código para mover un svg

--- applications/scadaclase/controllers/signals.py
+++ applications/scadaclase/controllers/signals.py
@@ -15,3 +15,15 @@ def registro():
    else:
        response.flash = 'por favor complete el formulario'
    return dict(formulario=formulario)
+
+def termometro():
+    return dict()
+
+
+def valor_potenciometro():
+    potenciometro = db(db.Signals.nombre=='Potenciómetro').select().first()
+    potenciometro_id = potenciometro.id
+
+    registro_potenciometro = db(db.Valores.direccion==potenciometro_id).select().last()
+
+    return dict(potenciometro=registro_potenciometro.valor)
